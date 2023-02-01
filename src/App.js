c6543707b7ba7e9b1363cf063e1f3769d86aea27
Hari Dulai <hdcodesdev@gmail.com>
2023-02-01 20:54:50 +0000

    Created propduct component, passing prop values

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Product from "./Product";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      {/* Product name, description, price */}
+      <Product
+        name="Amazon Echo"
+        description="Your AI assistant"
+        price={59.99}
+      />
     </div>
   );
 }
