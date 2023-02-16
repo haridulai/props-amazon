395d4efd0487b69757d8df064246f16f0ef72933
Harinder Dulai <harinder.dulai@wpnc.agency>
2023-02-16 20:18:17 +0000

    BEM naming styling

--- src/Product.js
+++ src/Product.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ItemDescription from "./ItemDescription";
+import "./Product.css";
 
 function Product({ name, description, price }) {
   return (
-    <div>
+    <div className="product">
       <ItemDescription name={name} description={description} />
-      <h3>${price}</h3>
+      <h2 className="product__error">An error occurred</h2>
+      <h3 className="product__price">${price}</h3>
     </div>
   );
 }
